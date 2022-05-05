a74805b481818e5ca7130636c9d1055fb900e08e
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-05 21:21:00 +0300

    Delete waste files

--- src/CMDArgsHelpers.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <ostream>
-#include "LaunchOptions.hpp"
-
-namespace Helpers {
-    LaunchOptions processCMDArgs(const int argc, const char* argv[]);
-    void outputHelp(std::ostream& stream);
-}
