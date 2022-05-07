412a32b5733cca11d76b9e01e54c7dd89585469b
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-07 17:30:13 +0300

    Update server

--- src/Server.hpp
+++ src/Server.hpp
@@ -22,8 +22,6 @@ protected:
 	boost::asio::ip::tcp::acceptor acceptor;
 	std::thread ioThread;
 
-	size_t nextClienID = 0;
-
 public:
 	Server(uint16_t port = 60'000) 
 		: acceptor{ ioContext, boost::asio::ip::tcp::endpoint{ boost::asio::ip::tcp::v4(), port} } {
@@ -68,11 +66,11 @@ public:
 					std::cout << "[SERVER]: New connection -- " << socket.remote_endpoint() << "\n";
 
 					std::shared_ptr<Connection<T>> newConnection = std::make_shared<Connection<T>>(Connection<T>::Owner::server,
-						ioContext, std::move(socket), msgIn);
+						ioContext, std::move(socket), msgIn, PossibleMessageIDs::check);
 
 					onClientConnect(newConnection);
 					connections.push_back(newConnection);
-					connections.back()->connectToClient(nextClienID++);
+					connections.back()->connectToClient();
 					std::cout << "[" << connections.back()->getID() << "] has been connected!\n";
 				} else {
 					std::cout << "[SERVER]: New connection has error -- " << ec.message() << "\n";
@@ -88,43 +86,22 @@ public:
 			client->send(msg);
 			return true;
 		} else {
-			onClientDisconnect(client);
-
-			if (client->getName() == "")
-				connections.erase(std::remove(connections.begin(), connections.end(), client), connections.end());
-			else
-				users.erase(client->getName());
-			client.reset();
-
 			return false;
 		}
 	}
 
 	void messageAllClients(const Message<T>& msg, std::shared_ptr<Connection<T>> ignoredClient = nullptr) {
-		std::vector<std::string> names;
-		bool needClear = false;
-
 		for (auto& client : connections) {
 			if (client && client->isConnected()) {
 				if (client != ignoredClient)
 					client->send(msg);
-			} else {
-				onClientDisconnect(client);
-				if (client->getName() != "")
-					names.push_back(client->getName());
-				client.reset();
-				needClear = true;
 			}
-		}
-
-		if (needClear) {
-			connections.erase(std::remove(connections.begin(), connections.end(), nullptr), connections.end());
-			for (std::string name : names)
-				users.erase(name);
 		}
 	}
 
 	void processInputMsgs(size_t maxNumMsgs = -1) {
+		msgIn.wait();
+
 		size_t i = 0;
 		while (i < maxNumMsgs && !msgIn.empty()) {
 			auto msg = msgIn.front();
@@ -137,11 +114,21 @@ public:
 
 protected:
 	void onClientConnect(std::shared_ptr<Connection<T>> client) {
-		std::cout << "[SERVER]: Client with ID " << client->getID() << " connected\n";
+		Message<PossibleMessageIDs> msg;
+		msg.header.id = PossibleMessageIDs::serverResponse;
+		messageClient(client, msg);
 	}
 
 	void onClientDisconnect(std::shared_ptr<Connection<T>> client) {
 		std::cout << "[SERVER]: Client with ID " << client->getID() << " disconnected\n";
+		if (client->getName() != "") {
+			std::cout << "\t* Send to " << client->getName() << " inf disconnected\n";
+
+			Message<PossibleMessageIDs> msg;
+			msg.header.id = PossibleMessageIDs::clientDisconnected;
+			msg << client->getName();
+			messageAllClients(msg, client);
+		}
 	}
 
 	void onMessage(std::shared_ptr<Connection<T>> client, Message<T>& msg) {
@@ -150,20 +137,22 @@ protected:
 			std::string name;
 
 			msg >> name;
-			std::cout << "Client send name -- " << name << "\n";
-
 			client->setName(name);
 			users[name] = client;
+			connections.erase(std::remove(connections.begin(), connections.end(), client), connections.end());
+
+			Message<PossibleMessageIDs> newClient;
+			newClient.header.id = PossibleMessageIDs::newClient;
+			newClient << name;
+			messageAllClients(newClient, client);
 			break;
 		}
 		case PossibleMessageIDs::sendMessageTo: {
 			std::string nameTo;
 			
 			msg >> nameTo;
-			std::cout << client->getName() << " send message to " << nameTo;
-
 			msg << client->getName();
-			if (users.find(nameTo) != users.end() && !messageClient(users[nameTo], msg)) {
+			if (users.find(nameTo) == users.end() || !messageClient(users[nameTo], msg)) {
 				Message<PossibleMessageIDs> msg;
 				msg.header.id = PossibleMessageIDs::notAvailable;
 				messageClient(client, msg);
@@ -197,6 +186,32 @@ protected:
 			messageClient(client, response);
 			break;
 		}
+		case PossibleMessageIDs::check:
+			checkConnections();
+			break;
+		}
+	}
+
+	void checkConnections() {
+		std::vector<std::string> names;
+
+		for (const auto& [name, connection] : users) {
+			if (connection) {
+				if (!connection->isConnected()) {
+					names.push_back(connection->getName());
+					onClientDisconnect(connection);
+				}
+			} else {
+				users.erase(name);
+			}
+		}
+
+		if (!names.empty()) {
+			for (std::string name : names) {
+				auto client = users[name];
+				client.reset();
+				users.erase(name);
+			}
 		}
 	}
 };
