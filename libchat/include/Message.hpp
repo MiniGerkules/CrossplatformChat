c5a9cf7b9a315016d38371dccaddb725e0de6309
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-19 21:18:03 +0300

    Add check for size of IDType type [libchat/include/Message.hpp]

--- libchat/include/Message.hpp
+++ libchat/include/Message.hpp
@@ -7,8 +7,11 @@
 
 template <typename IDType>
 struct MessageHeader {
-    IDType type;
-    uint32_t size;
+    static_assert(sizeof(IDType) == sizeof(uint8_t),
+                  "IDType doesn't have right size! Must be equal uint8_t!");
+
+    IDType typeOption;      // One of the options of IDType
+    uint32_t size;          // Size of this message (including MessageHeader)
 };
 
 #pragma pack(pop)
