f037ac0f7e58fca02494ae60999d48d2900dcf39
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-20 12:37:32 +0300

    Add interface for connections [libchat/include/Connection.hpp]

---
+++ libchat/include/Connection.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <atomic>
+#include <memory>
+#include <optional>
+
+#include "ConnectionDelegate.hpp"
+#include "UniversalMessage.hpp"
+
+class Connection {
+public:
+    std::atomic<bool> isAvailable = false;
+    std::weak_ptr<ConnectionDelegate> delegate;
+
+public:
+    Connection(std::weak_ptr<ConnectionDelegate> delegate) : delegate{ std::move(delegate) } {
+    }
+
+    virtual bool open() = 0;
+    virtual void close() = 0;
+    virtual void send(UniversalMessage message) = 0;
+    virtual std::optional<UniversalMessage> read() = 0;
+};
