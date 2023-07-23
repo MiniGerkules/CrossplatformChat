1ceb2f7f671c3e98ad864aa6f89b3521f9d889df
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-23 10:58:10 +0300

    Change types of methods of ConnectionDelegate class [libchat/include/ConnectionDelegate.hpp]

--- libchat/include/ConnectionDelegate.hpp
+++ libchat/include/ConnectionDelegate.hpp
@@ -4,6 +4,6 @@ class Connection;
 
 class ConnectionDelegate {
 public:
-    virtual void ifLostConnection(Connection connection) = 0;
-    virtual void ifDataIsAvailable(Connection connection) = 0;
+    virtual void ifLostConnection(const Connection &connection, std::string what) = 0;
+    virtual void ifDataIsAvailable(const Connection &connection) = 0;
 };
