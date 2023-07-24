6ee898ffd0c61cbc244a7b9e2199efb67d0c2e19
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-24 23:20:06 +0300

    Add a handler for messages of Message<BasicMessageType> type [libchat/include/BasicMessageHandler.hpp]

---
+++ libchat/include/BasicMessageHandler.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "MessageHandler.hpp"
+#include "TextHandlerDelegate.hpp"
+
+#include "BasicMessageType.hpp"
+#include "MessageTypesDescsGetters.hpp"
+
+class BasicMessageHandler : public MessageHandler {
+    std::weak_ptr<TextHandlerDelegate> delegate;
+
+//MARK: - Overrides methods of MessageHandler interface
+public:
+    bool canHandle(const UniversalMessage &message) override {
+        auto desc = MessageType::getDescription<BasicMessageType>();
+        return message.header.type == desc;
+    }
+
+    void handle(const UniversalMessage &message) override {
+        if (!canHandle(message)) return;
+
+        if (message.header.size != 0) {
+            if (auto delegatePtr = delegate.lock())
+                delegatePtr->handleText(reinterpret_cast<const char *>(message.data.data()));
+        }
+    }
+
+//MARK: - Constructor and methods
+public:
+    BasicMessageHandler(std::weak_ptr<TextHandlerDelegate> delegate = {})
+            : delegate{ std::move(delegate) } {
+    }
+
+    bool isHeartbeat(const UniversalMessage &message) {
+        return canHandle(message) &&
+               message.header.typeOption == static_cast<uint8_t>(BasicMessageType::HEARTBEAT);
+    }
+
+    bool isCheckApp(const UniversalMessage &message) {
+        return canHandle(message) &&
+               message.header.typeOption == static_cast<uint8_t>(BasicMessageType::CHECK_APP);
+    }
+
+    bool isError(const UniversalMessage &message) {
+        return canHandle(message) &&
+               message.header.typeOption == static_cast<uint8_t>(BasicMessageType::ERROR);
+    }
+};
