246153699682b06369e89980bbf782d533ed4c53
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-24 22:38:45 +0300

    Add type to generic header field and move comment further [libchat/include/Message.hpp]

--- libchat/include/Message.hpp
+++ libchat/include/Message.hpp
@@ -10,14 +10,14 @@ struct MessageHeader {
     static_assert(sizeof(IDType) == sizeof(uint8_t),
                   "IDType doesn't have right size! Must be equal uint8_t!");
 
-    IDType typeOption;          // One of the options of IDType
-    uint32_t size;              // Size of message body
+    IDType typeOption;              // One of the options of IDType
+    uint32_t size;                  // Size of message body
 };
 
 #pragma pack(pop)
 
 template <typename IDType>
 struct Message {
-    MessageHeader header;       // Message header
-    std::vector<uint8_t> data;  // Message body
+    MessageHeader<IDType> header;   // Message header
+    std::vector<uint8_t> data;      // Message body
 };
