22aaa37f15b796e14dc7afb8d15c9007f670ba14
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-24 23:13:09 +0300

    Add MessageHandler interface for handlers of different types of UniversalMessage [libchat/include/MessageHandler.hpp]

---
+++ libchat/include/MessageHandler.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "UniversalMessage.hpp"
+
+class MessageHandler {
+public:
+    virtual bool canHandle(const UniversalMessage &message) = 0;
+    virtual void handle(const UniversalMessage &message) = 0;
+};
