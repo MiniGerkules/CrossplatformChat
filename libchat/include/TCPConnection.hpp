f931ba4a268c44902ef3df14d935e4b558b71aa1
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-24 22:47:18 +0300

    Update names of private methods and remove unnecessary spaces [libchat/include/TCPConnection.hpp]

--- libchat/include/TCPConnection.hpp
+++ libchat/include/TCPConnection.hpp
@@ -9,7 +9,7 @@
 
 class TCPConnection : public Connection {
     boost::asio::ip::tcp::socket socket_;
-    
+
     TSQueue<UniversalMessage> inMessages_;
 
 public:
@@ -17,49 +17,49 @@ public:
                   std::weak_ptr<ConnectionDelegate> delegate = std::weak_ptr<ConnectionDelegate>())
             : Connection(std::move(delegate)), socket_{ std::move(socket) } {
         isAlive.store(socket_.is_open());
-        
+
         // Start async read from socket
-        boost::asio::co_spawn(socket_.get_executor(), asyncMessageReader(), boost::asio::detached);
+        boost::asio::co_spawn(socket_.get_executor(), asyncMessageReader_(), boost::asio::detached);
     }
-    
+
     bool open() override {
         if (socket_.is_open()) return true;
-        
+
         boost::system::error_code ec;
         socket_.open(boost::asio::ip::tcp::v4(), ec);
         const bool res = static_cast<bool>(ec);
-        
+
         isAlive.store(res);
         return res;
     }
-    
+
     void close() override {
         if (!socket_.is_open()) return;
-        
+
         isAlive.store(false);
         socket_.close();
     }
-    
+
     void send(UniversalMessage message) override {
         using boost::asio::detached;
-        
+
         if (!isAlive) return;
-        
-        co_spawn(socket_.get_executor(), asyncSendData(std::move(message)), detached);
+
+        co_spawn(socket_.get_executor(), asyncSendData_(std::move(message)), detached);
     }
-    
+
     std::optional<UniversalMessage> read() override {
         // There may be some messages when isAlive == false, so we don't check
         // that field
         return inMessages_.pop();
     }
-    
+
 private:
-    boost::asio::awaitable<void> asyncMessageReader() {
+    boost::asio::awaitable<void> asyncMessageReader_() {
         using namespace boost::asio;
-        
+
         UniversalMessage storage;
-        
+
         try {
             for (;;) {
                 co_await async_read(socket_,
@@ -69,18 +69,18 @@ private:
                 co_await async_read(socket_,
                                     buffer(storage.data),
                                     use_awaitable);
-                
+
                 inMessages_.push(std::move(storage));
                 dataAvailable_();
             }
         } catch (const std::exception &error) {
-            lostConnection(error.what());
+            lostConnection_(error.what());
         }
     }
-    
-    boost::asio::awaitable<void> asyncSendData(UniversalMessage message) {
+
+    boost::asio::awaitable<void> asyncSendData_(UniversalMessage message) {
         using namespace boost::asio;
-        
+
         try {
             co_await async_write(socket_,
                                  buffer(&message.header, sizeof(message.header)),
@@ -89,20 +89,20 @@ private:
                                  buffer(message.data),
                                  use_awaitable);
         } catch (const std::exception &error) {
-            lostConnection(error.what());
+            lostConnection_(error.what());
         }
     }
-    
+
 private:
-    void lostConnection(std::string errorMsg) {
+    void lostConnection_(std::string errorMsg) {
         if (auto delegatePtr = delegate.lock()) {
             delegatePtr->ifLostConnection(std::move(errorMsg));
         }
 
         close();
     }
-    
-    void dataAvailable() {
+
+    void dataAvailable_() {
         if (auto delegatePtr = delegate.lock()) {
             delegatePtr->ifDataIsAvailable();
         }
