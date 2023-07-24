fec3e5b1bbfc0a1adda840f4ff8b10e1bcc4a8da
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-24 20:12:09 +0300

    Change interface methods arguments [libchat/include/ConnectionDelegate.hpp]

--- libchat/include/ConnectionDelegate.hpp
+++ libchat/include/ConnectionDelegate.hpp
@@ -4,6 +4,6 @@ class Connection;
 
 class ConnectionDelegate {
 public:
-    virtual void ifLostConnection(const Connection &connection, std::string what) = 0;
-    virtual void ifDataIsAvailable(const Connection &connection) = 0;
+    virtual void ifLostConnection(const std::string &what) = 0;
+    virtual void ifDataIsAvailable() = 0;
 };
