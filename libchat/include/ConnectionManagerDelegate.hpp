57cb915b9b9ed9f5886bcc289d08a8a5fd223b88
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-30 15:04:37 +0300

    Make ConnectionManager and ConnectionManagerDelegate types templates [libchat/include/ConnectionManagerDelegate.hpp]

--- libchat/include/ConnectionManagerDelegate.hpp
+++ libchat/include/ConnectionManagerDelegate.hpp
@@ -2,11 +2,13 @@
 
 #include <string_view>
 
+template <typename Executor>
 class ConnectionManager;
 
+template <typename Executor>
 class ConnectionManagerDelegate {
 public:
-    virtual void ifLostConnection(const ConnectionManager &manager,
+    virtual void ifLostConnection(const ConnectionManager<Executor> &manager,
                                   const std::string_view errorMsg) = 0;
-    virtual void ifDataIsAvailable(const ConnectionManager &manager) = 0;
+    virtual void ifDataIsAvailable(const ConnectionManager<Executor> &manager) = 0;
 };
