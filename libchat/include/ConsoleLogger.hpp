2c70b95f7b3c9bc259208f809643c6470667c568
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-30 15:10:42 +0300

    Fix includes and indents [libchat/include/ConsoleLogger.hpp]

--- libchat/include/ConsoleLogger.hpp
+++ libchat/include/ConsoleLogger.hpp
@@ -1,21 +1,22 @@
 #pragma once
 
+#include <iostream>
 #include <mutex>
 
-#include "LoggerMessageType.hpp"
+#include "Logger.hpp"
 
 class ConsoleLogger : public Logger {
 private:
     static std::mutex outputMutex_;
-    
+
 public:
     void log(const char* message, const LoggerMessageType type) override {
         auto typeDesc = getMsgTypeDescription(type);
-        
+
         std::lock_guard lock{ ConsoleLogger::outputMutex_ };
         std::cout << typeDesc << ": " << message << "\n";
     }
-    
+
 private:
     std::string getMsgTypeDescription(const LoggerMessageType type) {
         switch (type) {
