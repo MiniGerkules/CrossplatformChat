7f80ea2828386182981c1d83464c09927a23c861
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-30 18:46:12 +0300

    Add getDescription function for RegularMessageType enum [libchat/include/RegularMessageType.hpp]

--- libchat/include/RegularMessageType.hpp
+++ libchat/include/RegularMessageType.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
+
+#include "MessageTypesGeneralFuncs.hpp"
 
 enum class RegularMessageType : uint8_t {
     SET_NAME,
     TEXT_MESSAGE,
     DISCONNECTED
 };
+
+namespace MessageType {
+    template <RegularMessageType>
+    std::string getDescription() {
+        return "RegularMessageType";
+    }
+}
