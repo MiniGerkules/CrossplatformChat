26f46ef95f46f97040d8a450344b438504bd1232
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-07-31 23:34:10 +0300

    Add Displayer base class [client/include/Displayer/Displayer.hpp]

---
+++ client/include/Displayer/Displayer.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <memory>
+
+#include <Messages/UniversalMessage.hpp>
+#include <Messages/Handlers/MessageHandler.hpp>
+
+class Displayer {
+public:
+    std::shared_ptr<MessageHandler> handlersChain;
+
+public:
+    virtual void display(UniversalMessage message) = 0;
+};
