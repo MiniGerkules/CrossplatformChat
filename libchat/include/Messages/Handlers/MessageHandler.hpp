9ed5b86602e4860ffa1a3da389da839c637a79b9
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-01 23:42:16 +0300

    Update MessageHandler [libchat/include/Messages/Handlers/MessageHandler.hpp]

--- libchat/include/Messages/Handlers/MessageHandler.hpp
+++ libchat/include/Messages/Handlers/MessageHandler.hpp
@@ -4,10 +4,13 @@
 
 #include "../UniversalMessage.hpp"
 
-class MessageHandler : public ResponderChainNode<MessageHandler> {
+class MessageHandler {
+protected:
+    std::unique_ptr<MessageHandler> next_;
+    
 public:
+    MessageHandler(std::unique_ptr<MessageHandler> next) : next_{ std::move(next) } { }
+    virtual ~MessageHandler() = default;
+    
     virtual bool handle(const UniversalMessage &message) = 0;
-
-private:
-    virtual bool canHandle_(const UniversalMessage &message) = 0;
 };
