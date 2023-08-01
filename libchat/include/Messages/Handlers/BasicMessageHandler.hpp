910f6a0194eb906a6994a69fba52d4b3e538bea7
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-01 23:43:30 +0300

    Update BasicMessageHandler to satisfy MessageHandler [libchat/include/Messages/Handlers/BasicMessageHandler.hpp]

--- libchat/include/Messages/Handlers/BasicMessageHandler.hpp
+++ libchat/include/Messages/Handlers/BasicMessageHandler.hpp
@@ -7,42 +7,48 @@
 #include "../Types/BasicMessageType.hpp"
 
 class BasicMessageHandler : public MessageHandler {
-    std::weak_ptr<TextHandlerDelegate> delegate;
+    std::weak_ptr<TextHandlerDelegate> delegate_;
 
 //MARK: - Overrides methods of MessageHandler interface
 public:
-    bool canHandle(const UniversalMessage &message) override {
-        auto desc = MessageType::getDescription<BasicMessageType>();
-        return std::strncmp(message.header.type, desc.data(), std::size(message.header.type)) == 0;
-    }
-
-    void handle(const UniversalMessage &message) override {
-        if (!canHandle(message)) return;
-
-        if (message.header.size != 0) {
-            if (auto delegatePtr = delegate.lock())
-                delegatePtr->handleText(reinterpret_cast<const char *>(message.data.data()));
+    bool handle(const UniversalMessage &message) override {
+        if (!canHandle_(message)) {
+            return next_->handle(message);
+        } else {
+            if (message.header.size != 0) {
+                if (auto delegatePtr = delegate_.lock())
+                    delegatePtr->handleText(reinterpret_cast<const char *>(message.data.data()));
+            }
+            
+            return true;
         }
     }
 
 //MARK: - Constructor and methods
 public:
-    BasicMessageHandler(std::weak_ptr<TextHandlerDelegate> delegate = {})
-            : delegate{ std::move(delegate) } {
+    BasicMessageHandler(std::unique_ptr<MessageHandler> next = {},
+                        std::weak_ptr<TextHandlerDelegate> delegate = {})
+            : MessageHandler{ std::move(next) }, delegate_{ std::move(delegate) } {
     }
 
     bool isHeartbeat(const UniversalMessage &message) {
-        return canHandle(message) &&
+        return canHandle_(message) &&
                message.header.typeOption == static_cast<uint8_t>(BasicMessageType::HEARTBEAT);
     }
 
     bool isCheckApp(const UniversalMessage &message) {
-        return canHandle(message) &&
+        return canHandle_(message) &&
                message.header.typeOption == static_cast<uint8_t>(BasicMessageType::CHECK_APP);
     }
 
     bool isError(const UniversalMessage &message) {
-        return canHandle(message) &&
+        return canHandle_(message) &&
                message.header.typeOption == static_cast<uint8_t>(BasicMessageType::ERROR);
+    }
+    
+private:
+    bool canHandle_(const UniversalMessage &message) {
+        auto desc = MessageType::getDescription<BasicMessageType>();
+        return std::strncmp(message.header.type, desc.data(), std::size(message.header.type)) == 0;
     }
 };
