014f8d8831b50d714a2b6f9122ea7a7d8e93f142
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-06 14:42:21 +0300

    Use new name of function [libchat/include/Messages/Hadnlers/BasicMessageHandler.hpp]

--- libchat/include/Messages/Handlers/BasicMessageHandler.hpp
+++ libchat/include/Messages/Handlers/BasicMessageHandler.hpp
@@ -1,48 +1,54 @@
 #pragma once
 
 #include "MessageHandler.hpp"
-#include "TextHandlerDelegate.hpp"
 
-#include "../Types/MessageTypesGeneralFuncs.hpp"
 #include "../Types/BasicMessageType.hpp"
 
+#include "Delegates/BasicMessageHandlerDelegate.hpp"
+
 class BasicMessageHandler : public MessageHandler {
-    std::weak_ptr<TextHandlerDelegate> delegate_;
+    std::weak_ptr<BasicMessageHandlerDelegate> delegate_;
 
 //MARK: - Overrides methods of MessageHandler interface
 public:
     bool handle(const UniversalMessage &message) override {
-        if (!MessageType::isUniversalMessageType<BasicMessageType>(message)) {
+        if (!MessageType::isMessageType<BasicMessageType>(message)) {
             return next_->handle(message);
-        } else {
-            if (message.header.size != 0) {
-                if (auto delegatePtr = delegate_.lock())
-                    delegatePtr->handleText(reinterpret_cast<const char *>(message.data.data()));
-            }
-            
-            return true;
         }
+
+        if (message.header.size != 0) {
+            if (auto delegatePtr = delegate_.lock()) {
+                if (isHeartbeat(message))
+                    delegatePtr->messageIsHeartbeat();
+                else if (isCheckApp(message))
+                    delegatePtr->messageIsCheck();
+                else
+                    delegatePtr->messageIsError(reinterpret_cast<const char *>(message.data.data()));
+            }
+        }
+
+        return true;
     }
 
 //MARK: - Constructor and methods
 public:
     BasicMessageHandler(std::unique_ptr<MessageHandler> next = {},
-                        std::weak_ptr<TextHandlerDelegate> delegate = {})
+                        std::weak_ptr<BasicMessageHandlerDelegate> delegate = {})
             : MessageHandler{ std::move(next) }, delegate_{ std::move(delegate) } {
     }
 
     bool isHeartbeat(const UniversalMessage &message) {
-        return MessageType::isUniversalMessageType<BasicMessageType>(message) &&
+        return MessageType::isMessageType<BasicMessageType>(message) &&
                message.header.typeOption == static_cast<uint8_t>(BasicMessageType::HEARTBEAT);
     }
 
     bool isCheckApp(const UniversalMessage &message) {
-        return MessageType::isUniversalMessageType<BasicMessageType>(message) &&
+        return MessageType::isMessageType<BasicMessageType>(message) &&
                message.header.typeOption == static_cast<uint8_t>(BasicMessageType::CHECK_APP);
     }
 
     bool isError(const UniversalMessage &message) {
-        return MessageType::isUniversalMessageType<BasicMessageType>(message) &&
+        return MessageType::isMessageType<BasicMessageType>(message) &&
                message.header.typeOption == static_cast<uint8_t>(BasicMessageType::ERROR);
     }
 };
