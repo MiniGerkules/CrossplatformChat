765fe61e523dd9dee242c90f20610a83c32267c5
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-15 12:20:40 +0300

    Add function for getting text from Message<T> [libchat/include/Messages/Types/MessageTypesFuncs.hpp]

--- libchat/include/Messages/Types/MessageTypesFuncs.hpp
+++ libchat/include/Messages/Types/MessageTypesFuncs.hpp
@@ -2,20 +2,39 @@
 
 #include <string_view>
 
+#include <Messages/Message.hpp>
 #include <Messages/UniversalMessage.hpp>
 
 namespace MessageType {
 
+namespace Details {
+
+std::string_view getTextFromVector(const std::vector<uint8_t> &data) {
+    if (data.size() != 0)
+        return reinterpret_cast<const char *>(data.data());
+    else
+        return std::string_view();
+}
+
+} // Details
+
 /// The function returns text from `message`.
 ///
 /// Use only for lvalue messages! std::string\_view **doesn't store** data!
-/// - Parameter message: the message whose text you want to receive
-/// - Returns: text from the `message`
+/// - Parameter message: The message whose text you want to receive
+/// - Returns: Text from the `message`
 std::string_view getTextFrom(const UniversalMessage &message) {
-    if (message.header.size != 0)
-        return reinterpret_cast<const char *>(message.data.data());
-    else
-        return std::string_view();
+    return Details::getTextFromVector(message.data);
+}
+
+template <typename IDType>
+/// The function returns text from `message`.
+///
+/// Use only for lvalue messages! std::string\_view **doesn't store** data!
+/// - Parameter message: The message whose text you want to receive
+/// - Returns: Text from the `message`
+std::string_view getTextFrom(const Message<IDType> &message) {
+    return Details::getTextFromVector(message.data);
 }
 
 } // MessageType
