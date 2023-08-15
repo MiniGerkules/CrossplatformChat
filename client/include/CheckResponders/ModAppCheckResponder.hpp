94595bcb04a5cf2116d4806303feb01442bffd7a
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-15 16:19:44 +0300

    Add realization of AppCheckResponder interface [client/include/CheckResponders/ModAppCheckResponder.hpp]

---
+++ client/include/CheckResponders/ModAppCheckResponder.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <charconv>
+
+#include <Messages/Types/MessageTypesFuncs.hpp>
+#include <Functions/ModFunction.hpp>
+
+#include "AppCheckResponder.hpp"
+
+class ModAppCheckResponder final : public AppCheckResponder {
+public:
+    Message_t createRespond(const Message_t& request) override {
+        auto text = MessageType::getTextFrom(request);
+        uint64_t requestNum;
+        auto [_, ec] = std::from_chars(text.data(), text.data() + text.size(), requestNum);
+
+        if (ec != std::errc())
+            throw std::exception();
+
+        uint64_t responseNum = CheckFunctions::modFunction(requestNum);
+        Message_t message = {
+            .header = {
+                .typeOption = BasicMessageType::CHECK_APP,
+                .size = sizeof(responseNum)
+            },
+            .data = std::vector<uint8_t>(sizeof(responseNum))
+        };
+
+        std::memcpy(message.data.data(), &responseNum, sizeof(responseNum));
+
+        return message;
+    }
+};
