dea78c9ee8fa1314fa8c16add41a7c509f3c0a2f
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-15 16:48:34 +0300

    Move functions from MessageTypesGeneralFuncs.hpp to MessageTypesFuncs.hpp [libchat/include/Messages/Types]

--- libchat/include/Messages/Types/MessageTypesFuncs.hpp
+++ libchat/include/Messages/Types/MessageTypesFuncs.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <string_view>
 
 #include "Messages/Message.hpp"
@@ -35,6 +36,74 @@ template <typename IDType>
 /// - Returns: Text from the `message`
 std::string_view getTextFrom(const Message<IDType> &message) {
     return Details::getTextFromVector(message.data);
+}
+
+} // MessageType
+
+
+namespace MessageType {
+
+template <typename MessageType>
+std::string getDescription() {
+    throw DescriptionException{ "There isn't that message type!" };
+    return "";
+}
+
+template <typename MessageType>
+UniversalMessageHeader convertToUniversal(const MessageHeader<MessageType> &header) {
+    UniversalMessageHeader universal = {
+        .size = header.size,
+        .typeOption = static_cast<uint8_t>(header.typeOption)
+    };
+
+    auto desc = getDescription<MessageType>();
+    std::strncpy(universal.type, desc.c_str(), std::size(universal.type));
+
+    return universal;
+}
+
+template <typename MessageType>
+UniversalMessage convertToUniversal(Message<MessageType> message) {
+    return UniversalMessage {
+        .header = convertToUniversal(message.header),
+        .data = std::move(message.data)
+    };
+}
+
+} // MessageType
+
+
+namespace MessageType {
+
+template <typename MessageType>
+bool isMessageType(const UniversalMessageHeader &header) {
+    auto desc = getDescription<MessageType>();
+    return std::strncmp(header.type, desc.data(), std::size(header.type)) == 0;
+}
+
+template <typename MessageType>
+bool isMessageType(const UniversalMessage &universal) {
+    return isMessageType<MessageType>(universal.header);
+}
+
+template <typename MessageType>
+std::optional<MessageHeader<MessageType>> convertToTyped(const UniversalMessageHeader &header) {
+    if (!isMessageType<MessageType>(header)) return std::nullopt;
+
+    return MessageHeader<MessageType> {
+        .typeOption = static_cast<MessageType>(header.typeOption),
+        .size = header.size
+    };
+}
+
+template <typename MessageType>
+std::optional<Message<MessageType>> convertToTyped(UniversalMessage universal) {
+    if (!isMessageType<MessageType>(universal)) return std::nullopt;
+
+    return Message<MessageType> {
+        .header = convertToTyped<MessageType>(universal.header),
+        .data = std::move(universal.data)
+    };
 }
 
 } // MessageType
