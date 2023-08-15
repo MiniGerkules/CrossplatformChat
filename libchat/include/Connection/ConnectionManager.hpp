881e8d74fd6cde9e6fcd7818668617d977842f4b
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-15 11:16:27 +0300

    Use the new exception when validate connection [libchat/include/Connection/ConnectionManager.hpp]

--- libchat/include/Connection/ConnectionManager.hpp
+++ libchat/include/Connection/ConnectionManager.hpp
@@ -9,6 +9,7 @@
 
 #include "Connection.hpp"
 #include "Delegates/ConnectionManagerDelegate.hpp"
+#include "Exceptions/ConnectionException.hpp"
 
 #include "../Messages/Handlers/BasicMessageHandler.hpp"
 
@@ -16,43 +17,46 @@ class ConnectionManager : public ConnectionDelegate,
                           public std::enable_shared_from_this<ConnectionManager> {
 public:
     std::weak_ptr<ConnectionManagerDelegate> delegate;
-    
+
 private:
     std::unique_ptr<Connection> connection_;
-    
+
     BasicMessageHandler handler_;
     TSQueue<UniversalMessage> inMessages_;
 
     boost::posix_time::millisec period_{ 1'000 };
     std::atomic<bool> isSendingHeartbeat_ = false;
-    
+
     //MARK: - Overrides methods of ConnectionDelegate interface
 public:
     void ifLostConnection(const std::string_view errorMessage) override {
         if (auto delegatePtr = delegate.lock()) {
             delegatePtr->ifLostConnection(*this, errorMessage);
         }
-        
+
         connection_->close();
     }
-    
+
     void ifDataIsAvailable() override {
         auto msg = connection_->read();
         if (!msg.has_value()) return;
-        
+
         if (!handler_.isHeartbeat(msg.value())) {
             inMessages_.push(std::move(msg.value()));
-            
+
             if (auto delegatePtr = delegate.lock()) {
                 delegatePtr->ifDataIsAvailable(*this);
             }
         }
     }
-    
+
     //MARK: - Constructor and methods
 public:
     ConnectionManager(std::unique_ptr<Connection> connection)
             : connection_{ std::move(connection) } {
+        if (connection == nullptr)
+            throw ConnectionException("The connection is unreachable!");
+
         connection_->delegate = this->weak_from_this();
         connection_->open();
     }
@@ -60,15 +64,15 @@ public:
     bool isAlive() const noexcept {
         return connection_->isAlive;
     }
-                              
+
     std::optional<UniversalMessage> read() noexcept {
         return inMessages_.pop();
     }
-                              
+
     void send(UniversalMessage message) noexcept {
         connection_->send(std::move(message));
     }
-                              
+
     void close() noexcept {
         connection_->close();
     }
@@ -98,11 +102,11 @@ private:
             }
         );
         boost::asio::deadline_timer timer{ executor };
-        
+
         while (isSendingHeartbeat_) {
             timer.expires_from_now(period_);
             co_await timer.async_wait(boost::asio::use_awaitable);
-            
+
             connection_->send(message);
         }
     }
