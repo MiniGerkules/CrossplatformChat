4a6f706389ce7fac5549051cefe5350769d1852f
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-15 10:46:10 +0300

    Remove unnecessary whitespaces [libchat/include/Messages/Handlers/MessageHandler.hpp]

--- libchat/include/Messages/Handlers/MessageHandler.hpp
+++ libchat/include/Messages/Handlers/MessageHandler.hpp
@@ -5,10 +5,10 @@
 class MessageHandler {
 protected:
     std::unique_ptr<MessageHandler> next_;
-    
+
 public:
     MessageHandler(std::unique_ptr<MessageHandler> next) : next_{ std::move(next) } { }
     virtual ~MessageHandler() = default;
-    
+
     virtual bool handle(const UniversalMessage &message) = 0;
 };
