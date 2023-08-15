9cf6e03c308b2eaf30c37f97be44c0a0e48435ca
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-08-15 15:59:18 +0300

    Wrap function to namespace [libchat/include/FunctionsForCheckingApp/ModAppCheckFunction.hpp]

--- libchat/include/FunctionsForCheckingApp/ModAppCheckFunction.hpp
+++ libchat/include/FunctionsForCheckingApp/ModAppCheckFunction.hpp
@@ -2,6 +2,10 @@
 
 #include <cstdint>
 
+namespace CheckFunctions {
+
 uint64_t modAppCheckFunction(const uint64_t x) {
     return ((x << 5) ^ 0xF0F0F0A0A0F0F0F0ull) % 13799;
 }
+
+} // CheckFunctions
